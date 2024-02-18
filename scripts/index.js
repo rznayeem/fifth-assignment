c03d19601088d4e5385a6493f16223b5cbc727c8
rznayeem <riyazulnayeem@gmail.com>
2024-02-18 09:39:56 +0600

    ticket selection interaction added

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,33 @@
 function scrollDownByBtn() {
   const scrollTo = document.getElementById('buy-ticket');
-  scrollTo.scrollIntoView();
+  scrollTo.scrollIntoView({ block: 'start', behavior: 'smooth' });
+}
+const seats = document.querySelectorAll('.seat-selector');
+const seatArray = [];
+
+for (const seat of seats) {
+  const selectedSeat = seat;
+  const array = [];
+  selectedSeat.addEventListener('click', function () {
+    array.push(selectedSeat);
+    if (array.length > 1) {
+      selectedSeat.removeEventListener();
+    }
+    if (seatArray.length === 3) {
+      enableButtonById('apply-btn');
+    }
+    const seatCountElement = document.getElementById('seat-count');
+    const seatCount = parseInt(seatCountElement.innerText);
+    const totalSeatCount = seatCount + 1;
+    setTextById('seat-count');
+    if (totalSeatCount <= 4) {
+      seatArray.push(selectedSeat);
+      seatCountElement.innerText = totalSeatCount;
+      setElementsById('price-list', selectedSeat.innerText);
+      setBgColorByID(selectedSeat);
+    } else {
+      alert("You can't buy more than 4 tickets!");
+    }
+    console.log('clicked');
+  });
 }
---
+++ scripts/utility.js
@@ -0,0 +1,31 @@
+function setBgColorByID(elementId) {
+  elementId.classList.toggle('bg-[#F7F8F8]');
+  elementId.classList.toggle('bg-[#1DD100]');
+}
+function setTextById(elementId) {
+  const element = document.getElementById(elementId);
+  const elementText = parseInt(element.innerText);
+  const setElementText = elementText + 1;
+  if (elementText < 4) {
+    element.innerText = setElementText;
+  }
+}
+function enableButtonById(elementId) {
+  const element = document.getElementById(elementId);
+  element.removeAttribute('disabled');
+}
+function setElementsById(elementId, value) {
+  const element = document.getElementById(elementId);
+  const td = document.createElement('td');
+  const p1 = document.createElement('p');
+  const p2 = document.createElement('p');
+  const p3 = document.createElement('p');
+
+  p1.innerText = value;
+  p2.innerText = 'Economoy';
+  p3.innerText = 550;
+  td.appendChild(p1);
+  td.appendChild(p2);
+  td.appendChild(p3);
+  element.appendChild(td);
+}
